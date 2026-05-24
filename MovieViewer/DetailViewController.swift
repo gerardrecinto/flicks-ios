8c116e80a46f2d3e7498fcf7a5d77aad478bcb2f
Gerard Louis Andres Recinto <grecinto@ucsd.edu>
2026-05-24 15:56:09 -0700

    fix: remove dead AFNetworking code, guard force-unwrapped URL optionals

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -10,80 +10,29 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-
     @IBOutlet weak var scrollView: UIScrollView!
-
     @IBOutlet weak var infoView: UIView!
     @IBOutlet weak var overviewLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var posterImageView: UIImageView!
-    var movie: NSDictionary!
 
+    var movie: NSDictionary?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
-        let title = movie["title"] as? String
-        titleLabel.text = title
-        let overview = movie["overview"]
-        overviewLabel.text = overview as? String
 
+        titleLabel.text = movie?["title"] as? String
+        overviewLabel.text = movie?["overview"] as? String
         overviewLabel.sizeToFit()
-        let baseUrl = "https://image.tmdb.org/t/p/w500"
 
-        if let posterPath = movie["poster_path"] as? String{
+        self.navigationItem.title = "Movies"
 
-
-            let smallImageUrl = URL(string: "https://image.tmdb.org/t/p/w45" + posterPath)
-            let largeImageUrl = URL(string: "https://image.tmdb.org/t/p/original" + posterPath)
-            let smallImageRequest = NSURLRequest(url: smallImageUrl!)
-            let largeImageRequest = NSURLRequest(url: largeImageUrl!)
-            self.navigationItem.title = "Movies"
-            if let navigationBar = navigationController?.navigationBar {
-                //  navigationBar.setBackgroundImage(UIImage(named: "star"), for: .default)
-                let shadow = NSShadow()
-                shadow.shadowColor = UIColor.gray.withAlphaComponent(0.5)
-                shadow.shadowOffset = CGSize(width: 2, height: 2)
-                // shadow.shadowOffset = CGSizeMake(2, 2)
-                shadow.shadowBlurRadius = 4
-                navigationBar.titleTextAttributes = [
-                    NSFontAttributeName : UIFont.boldSystemFont(ofSize: 22),
-                    NSForegroundColorAttributeName : UIColor(red: 0.5, green: 0.15, blue: 0.15, alpha: 0.8),
-                    NSShadowAttributeName : shadow
-                ]
-            }
-
-
-
-            self.posterImageView.loadImage(from: imageUrl!)
-                    })
-            },
-                failure: { (request, response, error) -> Void in
-                    // do something for the failure condition
-                    // possibly try to get the large image
-            })
-
-            //let imageUrl = URL(string: baseUrl + posterPath)
-            //posterImageView.loadImage(from: imageUrl!)
-
-
-
+        if let posterPath = movie?["poster_path"] as? String,
+           let imageUrl = URL(string: "https://image.tmdb.org/t/p/w500" + posterPath) {
+            self.posterImageView.loadImage(from: imageUrl)
         }
 
-        print(movie)
-        // Do any additional setup after loading the view.
+        print(movie as Any)
     }
-
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
